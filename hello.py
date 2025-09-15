562d9a3952d481f5c89614feb8137b3f3f6bd7b9
dkobelak457 <davidkobelak@gmail.com>
2025-09-15 11:40:16 -0400

    Update hello.py

--- hello.py
+++ hello.py
@@ -7,3 +7,6 @@
 # 3. Add another print statement with your favorite hobby.
 
 print("Hello, world!")
+
+print("Hello! My name is David Kobelak.")
+print("My favourite hobby is playing guitar, I am self taught and have been an active player over the last 3 years.")
